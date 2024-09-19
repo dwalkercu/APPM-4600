c3e2cc188417553c1a09b091929294803c1a11c1
Derek Walker <dewa9753@colorado.edu>
2024-09-19 10:53:47 -0600

    finished lab4

--- Labs/Lab 4/lab4.py
+++ Labs/Lab 4/lab4.py
@@ -27,36 +27,76 @@ def fixedpt_its(f,x0,tol,Nmax):
     ier = 1
     return None
 
-def order_of_convergence(phat):
-  if (type(phat) == np.ndarray and len(phat) < 3) or (type(phat) != np.ndarray):
-    return -1
+def fixedpt_steffensons(f, x0, tol, Nmax):
+    its = np.zeros(Nmax)
+    pn = x0
+    count = 0
 
-  alpha = 1
-  p = phat[-1]
-  pn1 = phat[-2]
-  pn = phat[-3]
-  nmax = 50
+    while (count < Nmax):
+      a = pn
+      its[count] = a
+      b = f(pn)
+      c = f(b)
 
+      if np.abs(a-b) < tol:
+        its = np.trim_zeros(its, 'b')
+        return its
+      
+      pn = a - ((b-a)**2) / (c - 2*b + a)
+      count += 1
+
+    return None
+
+def order_of_convergence(x, xstar):
+  diff1 = np.abs(x[1::]-xstar)
+  diff2 = np.abs(x[0:-1]-xstar)
+  fit = np.polyfit(np.log(diff2.flatten()), np.log(diff1.flatten()),1)
+  return (fit[0],np.exp(fit[1])) # (alpha, lambda)
+
+def delta_squared(seq, tol, nmax):
   count = 0
-  lda = 0
+  p = seq[-1]
+  its = np.zeros(nmax)
+
   while(count < nmax):
-    lda = (np.abs(pn1 - p)) / (np.abs(pn - p)**alpha)
-    if lda < 1 and lda >= 0:
-      return (alpha, lda)
-    alpha += 1
+    pn = seq[count]
+    pn1 = seq[count+1]
+    pn2 = seq[count+2]
+
+    phat = pn - (pn1 - pn)**2 / (pn2 - 2*pn1 + pn)
+    its = np.append(its, phat)
+
+    if(phat - p < tol):
+      its = np.trim_zeros(its, 'b')
+      if phat == 0:
+        its[-1] = 0
+      return its
+
     count += 1
+  
+  return None
 
-  return -1
-
-def test_func(x):
+def f1(x):
   return (10/(x+4))**(1/2)
 
 # 2.2
-its = fixedpt_its(test_func, 1.5, 10**-10, 50)
+its = fixedpt_its(f1, 1.5, 10**-10, 50)
 if type(its) != None:
-  print(order_of_convergence(its))
+  print(order_of_convergence(its, 1.3652300134140976))
 else:
   print("fixed pt didn't converge")
 
 
 # 3.2
+d2 = delta_squared(its, 10**-10, 50)
+if type(d2) != None:
+  print(order_of_convergence(d2, 1.3652300134140976))
+else:
+  print("error with delta_squared")
+
+# 3.4
+its_s = fixedpt_steffensons(f1, 1.5, 10**-10, 50)
+if type(its_s) != None:
+  print(order_of_convergence(its_s, 1.3652300134140976))
+else:
+  print("fixed pt steffenson's didn't converge")
